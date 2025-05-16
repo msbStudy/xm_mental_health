22202d6b45f8795bbc3eb054338a7756abdd5bcd
msb <1272853891@qq.com>
2025-05-17 03:02:25 +0800

    解决AiController.java合并冲突，合并bf44fea7到master

--- springboot/src/main/java/com/example/controller/AiController.java
+++ springboot/src/main/java/com/example/controller/AiController.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
